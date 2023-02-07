f3e4044230f1e42d363180eeda14eb104765db11
Thomas <nyumat18@gmail.com>
2023-02-07 02:32:16 -0800

    Initial Commit

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <h1>My React App</h1>
+    </div>
+  );
+};
+
+export default App;
