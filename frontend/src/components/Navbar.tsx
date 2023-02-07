9dfe597d10d40f50c1b6d1fd7a8048c15c483404
Thomas <nyumat18@gmail.com>
2023-02-07 04:24:42 -0800

    Added Some Fun Animations

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,46 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const sentence = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.08,
+      },
+    },
+  };
+
+  const letter = {
+    hidden: { opacity: 0, y: 40 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  const word = "Miscellaneous Open AI Playground";
+
   return (
     <>
       <nav className="flex items-center justify-between flex-wrap px-4 w-full h-16 bg-gradient-to-r from-purple-700 via-purple-400 to-violet-500">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="font-semibold text-xl tracking-tight">
-            Miscellaneous Open AI Chat Room
-          </span>
+          <motion.span
+            className="font-semibold text-xl tracking-tight"
+            variants={sentence}
+            initial="hidden"
+            animate="visible"
+          >
+            {word.split("").map((char, index) => {
+              return (
+                <motion.span key={index} variants={letter}>
+                  {char}
+                </motion.span>
+              );
+            })}
+          </motion.span>
         </div>
       </nav>
     </>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)]">
+    <motion.div
+      className="flex flex-col items-center justify-center h-[calc(100vh-4rem)]"
+      initial={{ y: -100, x: -700, opacity: 0, scale: 0.5 }}
+      animate={{ y: 0, x: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: 2, type: "spring" }}
+    >
       <h1 className="text-4xl text-white font-bold">Home</h1>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
@@ -13,7 +19,7 @@ const Home = () => {
       >
         Go to User Home
       </button>
-    </div>
+    </motion.div>
   );
 };
 
